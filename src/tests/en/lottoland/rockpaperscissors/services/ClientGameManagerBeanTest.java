e5a1c93c810301c7b99482c06a5f7717ad11dd93
Ztryx <not.andres.diaz@gmail.com>
2018-04-30 15:18:51 +0200

    Added test for ClientGameManagerBean

---
+++ src/tests/en/lottoland/rockpaperscissors/services/ClientGameManagerBeanTest.java
@@ -0,0 +1,46 @@
+package en.lottoland.rockpaperscissors.services;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by andyx on 30/4/18.
+ */
+public class ClientGameManagerBeanTest extends TestCase {
+    ClientGameManagerBean clientTestBean = new ClientGameManagerBean();
+
+    public void testInitClientGameManagerBean() throws Exception {
+        clientTestBean.initClientGameManagerBean();
+        assertNotNull(clientTestBean.getList());
+    }
+
+    public void testSetNewMatch() throws Exception, Throwable {
+        clientTestBean.initClientGameManagerBean();
+        int previousSize;
+        for(int i=0; i<1000; i++) {
+            previousSize = clientTestBean.getList().size();
+            clientTestBean.setNewMatch();
+            assertNotNull(clientTestBean.getList());
+            assertNotSame(previousSize, clientTestBean.getList().size());
+        }
+    }
+
+    public void testGetRounds() throws Exception {
+        clientTestBean.initClientGameManagerBean();
+        assertEquals(String.valueOf(0), clientTestBean.getRounds());
+    }
+
+    public void testReset() throws Exception, Throwable {
+        clientTestBean.initClientGameManagerBean();
+        for(int i=0; i<1000; i++)
+            clientTestBean.setNewMatch();
+        assertTrue(Integer.valueOf(clientTestBean.getRounds())>0);
+        clientTestBean.reset();
+        assertTrue(Integer.valueOf(clientTestBean.getRounds())==0);
+    }
+
+    public void testGetList() throws Exception {
+        clientTestBean.initClientGameManagerBean();
+        assertNotNull(clientTestBean.getList());
+    }
+
+}
---
+++ src/tests/en/lottoland/rockpaperscissors/services/ServerGameManagerBeanTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by andyx on 30/4/18.
+ */
+public class ServerGameManagerBeanTest extends TestCase {
+
+}
