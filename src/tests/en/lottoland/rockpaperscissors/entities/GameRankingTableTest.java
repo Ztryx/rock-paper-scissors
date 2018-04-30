7f830ff9c3f954384139399fc7522a7fa21b1cb1
Ztryx <not.andres.diaz@gmail.com>
2018-04-30 13:33:10 +0200

    Add template for testing methods from class GameRankingTableTest

--- src/tests/en/lottoland/rockpaperscissors/entities/GameRankingTableTest.java
+++ src/tests/en/lottoland/rockpaperscissors/entities/GameRankingTableTest.java
@@ -1,8 +1,33 @@
+package en.lottoland.rockpaperscissors.entities;
+
 import junit.framework.TestCase;
 
 /**
  * Created by andyx on 30/4/18.
  */
 public class GameRankingTableTest extends TestCase {
+    public void testGetPlayerOneWins() throws Exception {
+    }
+
+    public void testIncreasePlayerOne() throws Exception {
+    }
+
+    public void testGetPlayerTwoWins() throws Exception {
+    }
+
+    public void testIncreasePlayerTwo() throws Exception {
+    }
+
+    public void testGetDraws() throws Exception {
+    }
+
+    public void testIncreaseDraw() throws Exception {
+    }
+
+    public void testGetRounds() throws Exception {
+    }
+
+    public void testIncreaseRounds() throws Exception {
+    }
 
 }
