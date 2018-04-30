9ad8cf23d5459e7205533461c023585422e4b7d6
Ztryx <not.andres.diaz@gmail.com>
2018-04-30 13:32:49 +0200

    Add template for testing methods from class GameMatchTest

---
+++ src/tests/en/lottoland/rockpaperscissors/entities/GameMatchTest.java
@@ -0,0 +1,27 @@
+package en.lottoland.rockpaperscissors.entities;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by andyx on 30/4/18.
+ */
+public class GameMatchTest extends TestCase {
+    public void testGetPlayerOne() throws Exception {
+    }
+
+    public void testSetPlayerOne() throws Exception {
+    }
+
+    public void testGetPlayerTwo() throws Exception {
+    }
+
+    public void testSetPlayerTwo() throws Exception {
+    }
+
+    public void testGetResult() throws Exception {
+    }
+
+    public void testSetResult() throws Exception {
+    }
+
+}
