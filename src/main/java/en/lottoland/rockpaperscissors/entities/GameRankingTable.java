72240741141df2dde2f2e86d04a389d47a72039e
Ztryx <not.andres.diaz@gmail.com>
2018-04-30 13:30:49 +0200

    Entity GameRankingTable: will save 4 numbers, playerOne wins, playerTwo wins, draws and rounds played by general for all the clients

--- src/main/java/en/lottoland/rockpaperscissors/entities/GameRankingTable.java
+++ src/main/java/en/lottoland/rockpaperscissors/entities/GameRankingTable.java
@@ -1,7 +1,52 @@
 package en.lottoland.rockpaperscissors.entities;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by andyx on 30/4/18.
  */
 public class GameRankingTable {
+    private AtomicInteger playerOneWins;
+    private AtomicInteger playerTwoWins;
+    private AtomicInteger draws;
+    private AtomicInteger rounds;
+
+    public GameRankingTable() {
+        this.playerOneWins = new AtomicInteger(0);
+        this.playerTwoWins = new AtomicInteger(0);
+        this.draws = new AtomicInteger(0);
+        this.rounds = new AtomicInteger(0);
+    }
+
+    public int getPlayerOneWins() {
+        return playerOneWins.intValue();
+    }
+
+    public void increasePlayerOne() {
+        this.playerOneWins.incrementAndGet();
+    }
+
+    public int getPlayerTwoWins() {
+        return playerTwoWins.intValue();
+    }
+
+    public void increasePlayerTwo() {
+        this.playerTwoWins.incrementAndGet();
+    }
+
+    public int getDraws() {
+        return draws.intValue();
+    }
+
+    public void increaseDraw() {
+        this.draws.incrementAndGet();
+    }
+
+    public int getRounds() {
+        return rounds.intValue();
+    }
+
+    public void increaseRounds() {
+        this.rounds.incrementAndGet();
+    }
 }
